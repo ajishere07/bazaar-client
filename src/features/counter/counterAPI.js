218e16cbbc5371772b0c2902aa56fad02f504277
anuj <anujpy.acct@gmail.com>
2023-11-13 10:09:11 +0530

    deleted useless files and maintained a template for future feature folders

--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,6 +1,8 @@
 // A mock function to mimic making an async request for data
 export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
-  );
+  return new Promise(async (resolve) => {
+    const resp = await fetch("http://localhost:8000");
+    const res = await resp.json();
+    resolve({ data });
+  });
 }
