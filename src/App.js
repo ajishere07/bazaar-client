9b1863be9914d33a7c9911e0385440f0ca76ae04
anuj <anujpy.acct@gmail.com>
2023-11-15 10:13:37 +0530

    sign up and login page UI

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/Home";
 
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <SignupPage />
     </div>
   );
 }
